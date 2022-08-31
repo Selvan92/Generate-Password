2f7503eec6033a7b3a6b46eb5f1b96d96dd38336
Satya Selvan <satyaat92@gmail.com>
2022-08-31 13:24:34 +0800

    Code Structure added

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//ask the user how long is the password
+
+//ask if want to include
+//uppercase
+//lowercase
+//number
+//special symbols
+
+//generate password based on the criteias selected and length
+
+//1. create the charset based on the criterias
+
+//2. loop for the 'length' times, for each iterations, 
+
+//grab a random char and append to an accumulator
+
+
+
+
+
+//bank of chars
+
+//abcdeABCDE1234!@#$%^
+
+//length:6
+//loop to get the random characters
+
+//pw -- set f random characters
+
+
+
+//return the password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +69,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
